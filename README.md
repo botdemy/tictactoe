409d3b920b8ee6970e044e4548d3ff5676ba2d71
kevin <botdemy@gmail.com>
2020-01-19 08:59:34 -0800

    weekly update

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
-# tictactoe
+# TIC TAC TOE 
+### How to download:
+##### First Time
+```
+git clone https://github.com/botdemy/tictactoe.git
+```
+##### Weekly Refresh
+```
+cd tictactoe
+git fetch origin
+git reset --hard origin/master
+```
+### How to upload/commit: (admin only)
+```
+git add -A
+git commit -m "weekly update"
+git push
+```
---
+++ tictactoe.py
@@ -0,0 +1,6 @@
+#-------------------------------------------------------------------------------
+# Welcome to Tic-Tac-Toe Project
+# In this project, we'll write a Python code that lets you play Tic-Tac-Toe
+# against your friend or against a CPU
+#-------------------------------------------------------------------------------
+# Insert your code below
