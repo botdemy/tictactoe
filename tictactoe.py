5f55155afbb9d86bd89e404fc7808d61a2bf36cc
botdemy <30564866+botdemy@users.noreply.github.com>
2020-01-23 13:15:56 -0800

    Update tictactoe.py

--- tictactoe.py
+++ tictactoe.py
@@ -1,6 +1,6 @@
 #-------------------------------------------------------------------------------
 # Welcome to Tic-Tac-Toe Project
 # In this project, we'll write a Python code that lets you play Tic-Tac-Toe
-# against your friend or against a CPU
+# against your friend or against CPU
 #-------------------------------------------------------------------------------
 # Insert your code below
